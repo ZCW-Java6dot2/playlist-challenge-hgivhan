8f4fe869f3978d0ae2d4ef461814cc34126ea098
hillary <hillary@zipcodes-MBP.home>
2020-11-24 16:22:37 -0500

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,26 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int startIndexBack = startIndex;
+        int countDown = 0;
+        int countUp = 0;
+        while (!playList[startIndex].equals(selection)) {
+            startIndex++;
+            countUp++;
+            if (startIndex >= playList.length) {
+                startIndex = 0;
+            }
+        }
+        while(!playList[startIndexBack].equals(selection)){
+            startIndexBack--;
+            countDown++;
+            if(startIndexBack < 0){
+                startIndexBack = playList.length -1;
+            }
+        }
+
+
+        return (Math.min(countUp, countDown));
     }
 }
+//Math.min is fed 2 values and it tells you which is the least (to get to song with least presses)
